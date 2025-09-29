a83b8cf2c60b8af3601c21170b88e0ae0070480b
JakeVelicer <007velicer@gmail.com>
2025-09-29 18:07:59 -0500

    Switched Social Media Icons over to SVGs

--- js/TopLevelFooter.js
+++ js/TopLevelFooter.js
@@ -1,5 +1,5 @@
 fetch('../footer.html')
 .then(response => response.text())
 .then(data => {
-  document.getElementById("Footer").innerHTML = data;
+  document.getElementById("CurrentYear").textContent = new Date().getFullYear();
 });
